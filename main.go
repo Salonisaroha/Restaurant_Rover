431c7f8c48fd40418671018a99046041d247c146
Saloni Saroha <salonisaroha5@gmail.com>
2024-07-08 20:31:46 +0530

    changes done

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"flag"
+	// "flag"
 
-	"github.com/Salonisaroha/api"
+	// "github.com/Salonisaroha/api"
 	"github.com/gofiber/fiber/v2"
 )
 
-func main() {
-	listenAddar := flag.String("listenAddr", ":5000", "The listen address of the API server ")
-	flag.Parse()
-	app := fiber.New()
-	app.Get("/user", api.HandleGetUsers)
-	app.Get("/user/:id", api.HandleGetUser)
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "james foo"})
+}
+func handleFoo(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"msg": "this is working fyn"})
+}
 
-	app.Listen(*listenAddar)
+func main() {
+	// listenAddar := flag.String("listenAddr", ":5000", "The listen address of the API server ")
+	// flag.Parse()
+	// app := fiber.New()
+	// app.Get("/user", api.HandleGetUsers)
+	// app.Get("/user/:id", api.HandleGetUser)
+
+	// app.Listen(*listenAddar)
+
+	app := fiber.New()
+
+	appv1 := app.Group("/api/v1")
+	app.Get("/foo", handleFoo)
+	appv1.Get("/user", handleUser)
+	app.Listen(":5000")
 }
