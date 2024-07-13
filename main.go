a07b5766e2c25b1798cb0e963ee5f7007be8cac4
Saloni Saroha <salonisaroha5@gmail.com>
2024-07-13 20:46:16 +0530

    adding changes

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
-	//"github.com/Salonisaroha/api"
-
+	"github.com/Salonisaroha/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func handleUser(c *fiber.Ctx) error {
@@ -15,14 +19,28 @@ func handleFoo(c *fiber.Ctx) error {
 	return c.JSON(map[string]string{"msg": "this is working fyn"})
 }
 
-func main() {
-	// listenAddar := flag.String("listenAddr", ":5000", "The listen address of the API server ")
-	// flag.Parse()
-	// app := fiber.New()
-	// app.Get("/user", api.HandleGetUsers)
-	// app.Get("/user/:id", api.HandleGetUser)
+const dburi = "mongodb://localhost:27017"
+const dbname = "Hotel-reservation"
+const userColl = "users"
 
-	// app.Listen(*listenAddar)
+func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+
+	user := types.User{
+		FirstName: "James",
+		LastName:  "At the water cooler",
+	}
+	res, err := coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
 	app := fiber.New()
