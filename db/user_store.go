ee6f8b3230baf6ff205153bc086fc1b99b328f37
Saloni Saroha <salonisaroha5@gmail.com>
2024-12-28 12:24:06 +0530

    Commit validate changes

--- db/user_store.go
+++ db/user_store.go
@@ -10,31 +10,63 @@ import (
 )
 
 const userColl = "users"
+
 type UserStore interface {
-	GetUserByID(context.Context,string) (*types.User, error)
+	GetUserByID(context.Context, string) (*types.User, error)
+	GetUsers(context.Context) ([]*types.User, error)
+	InsertUser(context.Context, *types.User) (*types.User, error)
 }
-type MongoUserStore struct{
+
+type MongoUserStore struct {
 	client *mongo.Client
-	
-	coll *mongo.Collection
+	coll   *mongo.Collection
 }
-func NewMongoUserStore(client *mongo.Client)*MongoUserStore{
-	
+
+func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
 	return &MongoUserStore{
-		client:client,
-		coll : client.Database(DBNAME).Collection(userColl),
+		client: client,
+		coll:   client.Database(DBNAME).Collection(userColl),
 	}
 }
-// Gets the user by id
-func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error){
-	// validate the correctness of the ID
+
+func (s *MongoUserStore) InsertUser(ctx context.Context, user *types.User) (*types.User, error) {
+	res, err := s.coll.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	return user, nil
+}
+
+func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
+	cur, err := s.coll.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(ctx)
+
+	var users []*types.User
+	for cur.Next(ctx) {
+		var user types.User
+		if err := cur.Decode(&user); err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+	if err := cur.Err(); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
 	oid, err := primitive.ObjectIDFromHex(id)
-	if err!= nil{
+	if err != nil {
 		return nil, err
 	}
 	var user types.User
- if err :=  s.coll.FindOne(ctx, bson.M{"_id":oid}).Decode(&user); err != nil{
-	return nil, err
- }
- return &user, nil
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
