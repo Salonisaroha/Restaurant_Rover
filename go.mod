586f3d08c9f538a8940dd7ec5ca2ac837dd35ede
Saloni Saroha <salonisaroha5@gmail.com>
2024-12-28 12:26:29 +0530

    Added changes

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/Salonisaroha
 
 go 1.21.4
 
-require github.com/gofiber/fiber/v2 v2.52.5
+require (
+	github.com/gofiber/fiber/v2 v2.52.5
+	go.mongodb.org/mongo-driver v1.17.1
+	golang.org/x/crypto v0.31.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
@@ -20,10 +24,8 @@ require (
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.16.0 // indirect
-	golang.org/x/crypto v0.24.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,46 +2,47 @@ package main
 
 import (
 	"context"
-	
 	"flag"
-	
-
 	"log"
 
 	"github.com/Salonisaroha/api"
 	"github.com/Salonisaroha/db"
-
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-
-
 const dburi = "mongodb://localhost:27017"
 const dbname = "Hotel-reservation"
-const userColl = "users"
+
 var config = fiber.Config{
- ErrorHandler: func(c *fiber.Ctx, err error) error{
-	return c.JSON(map[string] string{"error" : err.Error()})
- },
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		log.Printf("Error: %v\n", err)
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
 }
+
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
+
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
 		log.Fatal(err)
 	}
-	// handlers initialization
-	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
-    
-	app := fiber.New(config)
 
+	// Handlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	appv1 := app.Group("/api/v1")
 
-	
-	app.Get("/user", userHandler.HandleGetUsers)
-	appv1.Get("/user;id", userHandler.HandleGetUser)
-	app.Listen(*listenAddr)
+	appv1.Post("/user", userHandler.HandlePostUser)
+	appv1.Get("/users", userHandler.HandleGetUsers)   // Fetch all users
+	appv1.Get("/user/:id", userHandler.HandleGetUser) // Fetch single user by ID
+
+	// Start the server
+	if err := app.Listen(*listenAddr); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
