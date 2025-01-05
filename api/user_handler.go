c46cc90cb1708533fc6a0dbe774a42717bbfd843
Saloni Saroha <salonisaroha5@gmail.com>
2025-01-05 21:17:00 +0530

    Handle Different requests from user

--- api/user_handler.go
+++ api/user_handler.go
@@ -22,17 +22,18 @@ func NewUserHandler(userStore db.UserStore) *UserHandler {
 }
 func(h *UserHandler) HandlePutUser(c *fiber.Ctx)error{
  var (
-	values bson.M
+	//values bson.M
+	params types.UpdateUserParams
 	userID = c.Params("id")
 )
 oid, err:= primitive.ObjectIDFromHex(userID)
 if err != nil{return err}
- if err := c.BodyParser(&values); err != nil{
+ if err := c.BodyParser(&params); err != nil{
 	return err
  }
  filter := bson.M{"_id":oid}
  
- if err := h.userStore.UpdateUser(c.Context(), filter, values); err != nil{
+ if err := h.userStore.UpdateUser(c.Context(), filter, params); err != nil{
 	return err
  }
  return c.JSON(map[string]string{"updated":userID})
