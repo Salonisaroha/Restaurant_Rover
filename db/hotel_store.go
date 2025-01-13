d5d397029028c2047a902cd2d1bb6be4c1f19745
Saloni Saroha <salonisaroha5@gmail.com>
2025-01-13 10:02:16 +0530

    added some changes

--- db/hotel_store.go
+++ db/hotel_store.go
@@ -4,12 +4,14 @@ import (
 	"context"
 
 	"github.com/Salonisaroha/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type HotelStore interface {
 	InsertedHotel(context.Context, *types.Hotel) (*types.Hotel, error)
+	Update (context.Context, bson.M, bson.M) error
 }
 type MongoHotelStore struct{
 	client *mongo.Client
@@ -21,7 +23,11 @@ func NewMongoHotelStore(client *mongo.Client, dbname string) *MongoHotelStore{
 		coll:client.Database(dbname).Collection("hotels"),
 	}
 }
-
+func(s *MongoHotelStore) Update (ctx context.Context, filter bson.M, update bson.M)error{
+ _, err := s.coll.UpdateOne(ctx, filter, update)
+ 
+ return err
+}
 func(s *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (*types.Hotel, error){
 	resp, err := s.coll.InsertOne(ctx, hotel)
 	if err!= nil{
